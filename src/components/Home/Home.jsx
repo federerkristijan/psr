466084842c5852e59cefd2c345de29ef3026928a
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-10 23:26:21 +0200

    adding coordinates

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,10 @@ const Home = () => {
   return (
     <div className='Home'>
       <img src={PartyStore} alt='home' usemap="#workmap" width="100%" height="550vh" />
+      {/* https://www.image-map.net/ */}
+      <area shape="circle" coords="25, 25, 75" href="/team.html" alt="Team page"></area>
+      <area shape="circle" coords="25, 25, 75" href="/media.html" alt="MediaHub page"></area>
+      <area shape="circle" coords="25, 25, 75" href="/team.html" alt="Team page"></area>
     </div>
   )
 }
