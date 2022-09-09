3e54762472b3c2b9118a6725b936ddf6b8229601
federerkristijan <federer.kristijan@gmail.com>
2022-09-09 23:35:48 +0200

    app.js router done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import { Router, Route, Link, Routes } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 
-import './App.css';
-import Home from './components/Home/Home';
+import "./App.css";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Events from "./components/Events/Events";
+import Home from "./components/Home/Home";
+import Impressum from "./components/Impressum/Impressum";
+import Media from "./components/Media/Media";
+import Partners from "./components/Partners/Partners";
+import Releases from "./components/Releases/Releases";
+import Shop from "./components/Shop/Shop";
+import Team from "./components/Team/Team";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes><Home /></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-        <Routes></Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Layout/>} >
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/events" element={<Events />}/>
+          <Route path="/impressum" element={<Impressum />}/>
+          <Route path="/media" element={<Media />}/>
+          <Route path="/partners" element={<Partners />}/>
+          <Route path="/releases" element={<Releases />}/>
+          <Route path="/shop" element={<Shop />}/>
+          <Route path="/team" element={<Team />}/>
+        </Route>
+      </Routes>
     </div>
   );
+}
+
+function Layout() {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
 }
 
 export default App;
