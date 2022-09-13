74d80e14c555f70fdaa467ad3d525015d278eaf7
federerkristijan <federer.kristijan@gmail.com>
2022-09-14 00:45:17 +0200

    sanity/schemas/about.js added

---
+++ sanity/schemas/about.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'about',
+  type: 'document',
+  title: 'About',
+  fields: [
+    {
+      name: 'story',
+      type: 'text',
+      title: 'Founding story'
+    },
+    {
+      name: 'chris-profile',
+      type: 'text',
+      title: 'Chris-Profile'
+    },
+    {
+      name: 'marty-profile',
+      type: 'text',
+      title: 'Marty-Profile'
+    }
+  ]
+}
