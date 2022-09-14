1ccefd2271b39d77e21f17c8c3277211966067ef
federerkristijan <federer.kristijan@gmail.com>
2022-09-15 00:07:13 +0200

    need keys, icons, css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import sanityClient from "../../lib/client";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { brands } from "@fortawesome/free-brands-svg-icons";
+import Insta from "../../assets/images/instagram.svg";
+import Face from "../../assets/images/facebook.svg";
 
 const About = () => {
   const [about, setAbout] = useState(null);
@@ -23,27 +23,30 @@ const About = () => {
     <div className="about">
       {about &&
         about.map((about) => (
-          <div className="about-data">
-            <div className="about-story">
+          <div className="about-data" >
+            <div className="about-story" key={about.story}>
               <h3>Founding Story</h3>
               <span>{about.story}</span>
             </div>
-            <div className="about-chris">
+            <div className="about-chris" key={about.chris_profile}>
               <div className="chris-text">
+                <h3>About Chris</h3>
                 <span>{about.chris_profile}</span>
               </div>
               <div className="chris-icons">
-                <FontAwesomeIcon icon="fab fa-instagram" />
-                <FontAwesomeIcon icon="fa-brands fa-facebook" />
+                {/* <FontAwesomeIcon icon="fab fa-instagram" /> */}
+                <i src={Insta} path="/" />
+                {/* <FontAwesomeIcon icon="fa-brands fa-facebook" /> */}
               </div>
             </div>
-            <div className="about-marty">
+            <div className="about-marty" key={about.marty_profile}>
               <div className="marty-text">
+                <h3>About Marty</h3>
                 <span>{about.marty_profile}</span>
               </div>
               <div className="marty-icons">
-                <FontAwesomeIcon icon="fab fa-instagram" />
-                <FontAwesomeIcon icon="fa-brands fa-facebook" />
+                {/* <FontAwesomeIcon icon="fab fa-instagram" />
+                <FontAwesomeIcon icon="fa-brands fa-facebook" /> */}
               </div>
             </div>
           </div>
