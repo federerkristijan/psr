49e228d6019784e8c8c70c0df86bd18a33a7c833
federerkristijan <federer.kristijan@gmail.com>
2022-09-14 23:27:20 +0200

    dom created, imported sanityClient. need defining slug

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import sanityClient from "../../lib/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/free-brands-svg-icons";
 
 const About = () => {
   const [about, setAbout] = useState(null);
@@ -26,16 +28,22 @@ const About = () => {
               <span>{about.story}</span>
             </div>
             <div className="about-chris">
-              {/* <span>{about.chris_profile}</span> */}
-              <span>
-
-              </span>
+              <div className="chris-text">
+                <span>{about.chris_profile}</span>
+              </div>
+              <div className="chris-icons">
+                <FontAwesomeIcon icon="fab fa-instagram" />
+                <FontAwesomeIcon icon="fa-brands fa-facebook" />
+              </div>
             </div>
             <div className="about-marty">
-              {/* <span>{about.marty_profile}</span> */}
-              <span>
-                
-              </span>
+              <div className="marty-text">
+                <span>{about.marty_profile}</span>
+              </div>
+              <div className="marty-icons">
+                <FontAwesomeIcon icon="fab fa-instagram" />
+                <FontAwesomeIcon icon="fa-brands fa-facebook" />
+              </div>
             </div>
           </div>
         ))}
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { SanityClient } from "@sanity/client";
+import { sanityClient } from "@sanity/client";
 
 const Events = () => {
   const [events, setEvents] = useState(null);
