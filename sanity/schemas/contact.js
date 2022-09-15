9d907205993fb94c394bd3870d907aaa9719d961
federerkristijan <federer.kristijan@gmail.com>
2022-09-15 10:16:00 +0200

    dom and client done, need additional css

---
+++ sanity/schemas/contact.js
@@ -0,0 +1,17 @@
+export default {
+  name: "contact",
+  type: "document",
+  title: "Contact",
+  fields: [
+    {
+      name: "address",
+      type: "string",
+      title: "Address"
+    },
+    {
+      name: "email",
+      type: "string",
+      title: "Email"
+    }
+  ]
+}
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import sanityClient from "../../lib/client";
+
+import "./Contact.css";
 
 const Contact = () => {
+  const [contact, setContact] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "contact"]{
+        address,
+        email
+        }`
+      )
+      .then((data) => setContact(data))
+      .catch(console.error);
+  }, []);
+
   return (
-    <div>{console.log(Contact)}</div>
+    <div className='contact'>
+      {contact && contact.map((item) => (
+      <div className="contact-data">
+      <div className="contact-address">
+        <span>{item.address}</span>
+      </div>
+      <div className="contact-email">
+        <span>{item.email}</span>
+      </div>
+      </div>
+      ))}
+    </div>
   )
 }
 
