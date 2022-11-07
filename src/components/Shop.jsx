25418e47e3850007c134e5d85d68a213e24939ab
federerkristijan <federer.kristijan@gmail.com>
2022-11-07 01:01:12 +0100

    record card - tracks and text aligned. need price

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -55,41 +55,40 @@ const Shop = () => {
       {shop &&
         shop.map((item) => (
           <div className="shop-data" key={item.title}>
-                <div className="record-cover">
-                  <img
-                    src={urlFor(item.cover).width(140).url()}
-                    alt={item.title}
-                  />
-                </div>
-                <div className="record-text">
-                  <div className="record-title">
-                    <h3>{item.artist}</h3>
-                    <h4>{item.title}</h4>
-                  </div>
-                </div>
-                <div className="price">
-                  <p>{item.price}€</p>
-                </div>
-                {/* multi file player, for now it just puts out as much players as there are files */}
-                <div className="tracks">
-                  {item.multiTrack
-                    ? Object.keys(item.multiTrack).length < 2
-                      ? "one song"
-                      : item.multiTrack.map((song) => (
-                          <ReactAudioPlayer
-                            src={
-                              song
-                                ? `https://cdn.sanity.io/files/pyenle2m/production/${song.asset._ref
-                                    .toString()
-                                    .slice(5)
-                                    .replace("-", ".")}`
-                                : "nope"
-                            }
-                            controls
-                          />
-                        ))
-                    : "no songs available on multi files"}
-                </div>
+            <div className="record-cover">
+              <img src={urlFor(item.cover).width(140).url()} alt={item.title} />
+            </div>
+            <div className="record-text">
+              <div className="record-artist">
+                <h3>{item.artist}</h3>
+              </div>
+              <div className="record-title">
+                <h4>{item.title}</h4>
+              </div>
+            </div>
+            <div className="price">
+              <p>{item.price}€</p>
+            </div>
+            {/* multi file player, for now it just puts out as much players as there are files */}
+            <div className="tracks">
+              {item.multiTrack
+                ? Object.keys(item.multiTrack).length < 2
+                  ? "one song"
+                  : item.multiTrack.map((song) => (
+                      <ReactAudioPlayer
+                        src={
+                          song
+                            ? `https://cdn.sanity.io/files/pyenle2m/production/${song.asset._ref
+                                .toString()
+                                .slice(5)
+                                .replace("-", ".")}`
+                            : "nope"
+                        }
+                        controls
+                      />
+                    ))
+                : "no songs available on multi files"}
+            </div>
           </div>
         ))}
     </div>
