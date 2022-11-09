939c22e7c833a7624781d85c611dde550921a81e
federerkristijan <federer.kristijan@gmail.com>
2022-11-09 15:39:56 +0100

    backButton.jsx created, with NavLink

---
+++ src/components/Back.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+
+const Back = () => {
+  return (
+    <div className='back-btn'>
+      <NavLink href="/"/>
+    </div>
+  )
+}
+
+export default Back
