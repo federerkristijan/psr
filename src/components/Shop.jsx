fc27513707abea304c55d6de919385ea4b1fb95f
federerkristijan <federer.kristijan@gmail.com>
2022-11-10 15:22:54 +0100

    code cleaned, all working

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../lib/client";
 import imageUrlBuilder from "@sanity/image-url";
-import ReactAudioPlayer from "react-audio-player";
+// import ReactAudioPlayer from "react-audio-player";
 import { AudioContextProvider } from "../elements/AudioContext";
 
 // credits to justinmc @https://github.com/justinmc/react-audio-player
@@ -47,8 +47,10 @@ const Shop = () => {
   console.log("tracks", shop);
 
   return (
+    <>
     <AudioContextProvider>
       <div className="shop">
+        <Back />
         <div className="shop-header">
           <div className="shop-title">
             <h1>Shop</h1>
@@ -59,59 +61,33 @@ const Shop = () => {
               <AiOutlineShoppingCart />
             </Link>
           </div>
-    <div className="shop">
-      <Back />
-      <div className="shop-header">
-        <div className="shop-title">
-          <h1>Shop</h1>
-          <h3>check out our collection</h3>
         </div>
-        <div className="cart">
-          <Link to="/shop/cart">
-            <AiOutlineShoppingCart />
-          </Link>
-        </div>
-        {shop &&
-          shop.map((item) => (
-            <div className="shop-data" key={item.title}>
-              <div className="record-cover">
-                <img
-                  src={urlFor(item.cover).width(140).url()}
-                  alt={item.title}
-                />
-              </div>
-              <div className="record-text">
-                <div className="record-artist">
-                  <h3>{item.artist}</h3>
+          {shop &&
+            shop.map((item) => (
+              <div className="shop-data" key={item.title}>
+                <div className="record-cover">
+                  <img
+                    src={urlFor(item.cover).width(140).url()}
+                    alt={item.title}
+                  />
                 </div>
-                <div className="record-title">
-                  <h4>{item.title}</h4>
+                <div className="record-text">
+                  <div className="record-artist">
+                    <h3>{item.artist}</h3>
+                  </div>
+                  <div className="record-title">
+                    <h4>{item.title}</h4>
+                  </div>
                 </div>
-              </div>
-              <div className="price">
-                <p>{item.price}€</p>
-              </div>
-              {/* multi file player, for now it just puts out as much players as there are files */}
-              <div className="tracks">
-                {item.multiTrack.map((song) => (
-                  <div>
-                    {song.artist}
-                    {/* <ReactAudioPlayer
-                    src={
-                      song
-                        ? `https://cdn.sanity.io/files/pyenle2m/production/${song.asset._ref
-                            .toString()
-                            .slice(5)
-                            .replace("-", ".")}`
-                        : "nope"
-                    }
-                    type="audio/mp3"
-                    preload="auto"
-                    play="true"
-                    className="audio_volume_only"
-                  ></ReactAudioPlayer> */}
-                    <HomeMadeAudioPlayer
-                      onclick={clickCheck}
+                <div className="price">
+                  <p>{item.price}€</p>
+                </div>
+                {/* multi file player, for now it just puts out as much players as there are files */}
+                <div className="tracks">
+                  {item.multiTrack.map((song) => (
+                    <div>
+                      {song.artist}
+                      {/* <ReactAudioPlayer
                       src={
                         song
                           ? `https://cdn.sanity.io/files/pyenle2m/production/${song.asset._ref
@@ -120,14 +96,30 @@ const Shop = () => {
                               .replace("-", ".")}`
                           : "nope"
                       }
-                    />
-                  </div>
-                ))}
+                      type="audio/mp3"
+                      preload="auto"
+                      play="true"
+                      className="audio_volume_only"
+                    ></ReactAudioPlayer> */}
+                      <HomeMadeAudioPlayer
+                        onclick={clickCheck}
+                        src={
+                          song
+                            ? `https://cdn.sanity.io/files/pyenle2m/production/${song.asset._ref
+                                .toString()
+                                .slice(5)
+                                .replace("-", ".")}`
+                            : "nope"
+                        }
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
       </div>
     </AudioContextProvider>
+    </>
   );
 };
 
