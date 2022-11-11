b8ca1d66e4530e5d432455d81300f74042f34569
federerkristijan <federer.kristijan@gmail.com>
2022-11-11 21:47:05 +0100

    .tracks > .track aligned. need tracks within recordCard

--- src/views/Shop.jsx
+++ src/views/Shop.jsx
@@ -81,8 +81,7 @@ const Shop = () => {
                 {/* multi file player, for now it just puts out as much players as there are files */}
                 <div className="tracks">
                   {item.multiTrack.map((song) => (
-                    <div>
-                      {song.artist}
+                    <div className="track">
                       <HomeMadeAudioPlayer
                         onclick={clickCheck}
                         src={
@@ -94,6 +93,7 @@ const Shop = () => {
                             : "nope"
                         }
                       />
+                      {song.artist}
                     </div>
                   ))}
                 </div>
