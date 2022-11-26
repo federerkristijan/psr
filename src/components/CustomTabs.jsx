e23801f95aff780df3918d305ba5a3832b108826
federerkristijan <federer.kristijan@gmail.com>
2022-11-26 10:40:46 +0100

    .nav inline fixed

--- src/components/CustomTabs.jsx
+++ src/components/CustomTabs.jsx
@@ -17,25 +17,14 @@ const CustomTabs = () => {
     <div className="Tabs-wrapper">
       <div className="Tabs">
         {/* tabs nav */}
-        <ul
-          className="Nav"
-          style={{
-            listStyle: "none",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            border: "1px solid blue",
-          }}
-        >
+        <ul className="Nav">
           <li
-            style={{ width: "30%", border: "1px solid red" }}
             className={activeTab === "lp" ? "active" : ""}
             onClick={handleLP}
           >
             LP
           </li>
           <li
-            style={{ width: "30%", border: "1px solid red" }}
             className={activeTab === "digital" ? "active" : ""}
             onClick={handleDigital}
           >
