4d724bea4c953fec3c71707f13771b043722e82d
federerkristijan <federer.kristijan@gmail.com>
2022-12-02 20:24:02 +0100

    home icon

--- src/lib/navData.js
+++ src/lib/navData.js
@@ -1,9 +1,13 @@
 // icons for home, about, events, team, media, partners, shop, about, contact, impressum, login
+import Home from "../assets/icons/home_icon.png"
 
-const navData = () => {
-  return (
-    <div>navData</div>
-  )
-}
+const navData = [
+  {
+    id: 0,
+    icon: <Home />,
+    text: "Home",
+    link: "/"
+  }
+]
 
 export default navData
