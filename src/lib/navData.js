3449407017f575513429b47467cf2577e1e2a58e
federerkristijan <federer.kristijan@gmail.com>
2022-12-02 20:21:10 +0100

    navData

---
+++ src/lib/navData.js
@@ -0,0 +1,9 @@
+// icons for home, about, events, team, media, partners, shop, about, contact, impressum, login
+
+const navData = () => {
+  return (
+    <div>navData</div>
+  )
+}
+
+export default navData
