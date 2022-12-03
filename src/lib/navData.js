32e8c1eb6aa65e13eef53933f89d09a8990926a3
federerkristijan <federer.kristijan@gmail.com>
2022-12-03 08:53:25 +0100

    icons imported and objects created

--- src/lib/navData.js
+++ src/lib/navData.js
@@ -1,5 +1,12 @@
 // icons for home, about, events, team, media, partners, shop, about, contact, impressum, login
-import Home from "../assets/icons/home_icon.png"
+// credits to "https://www.flaticon.com/free-icons/event" Event icons created by Freepik - Flaticon and "https://iconscout.com/icons/impressum" WEBTECHOPS LLP
+import Home from "../assets/icons/home_icon.png";
+import Contact from "../assets/icons/contact.png";
+import Event from "../assets/icons/event.png";
+import Media from "../assets/icons/media.png";
+import Partners from "../assets/icons/Partners.png";
+import Team from "../assets/icons/team.png";
+import Impressum from "../assets/icons/impressum.png";
 
 const navData = [
   {
@@ -7,7 +14,43 @@ const navData = [
     icon: <Home />,
     text: "Home",
     link: "/"
-  }
+  },
+  {
+    id: 1,
+    icon: <Contact />,
+    text: "Contact",
+    link: "/"
+  },
+  {
+    id: 2,
+    icon: <Event />,
+    text: "Event",
+    link: "/"
+  },
+  {
+    id: 3,
+    icon: <Media />,
+    text: "Media",
+    link: "/"
+  },
+  {
+    id: 4,
+    icon: <Partners />,
+    text: "Partners",
+    link: "/"
+  },
+  {
+    id: 5,
+    icon: <Team />,
+    text: "Team",
+    link: "/"
+  },
+  {
+    id: 6,
+    icon: <Impressum />,
+    text: "Impressum",
+    link: "/"
+  },
 ]
 
 export default navData
