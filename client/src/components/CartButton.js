668a11f7eb8460adde9f46f0658d398e5c191053
federerkristijan <federer.kristijan@gmail.com>
2023-02-16 21:13:13 +0100

    CartButton.js done - not rendering. error: CartContext

--- client/src/components/CartButton.js
+++ client/src/components/CartButton.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-const CartButton = () => {
+import { CartContext } from "../store/CartContext";
+import "../styles/global.css";
+
+const CartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  const { items } = useContext(CartContext);
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${"cart-btn"} ${btnIsHighlighted} ? ${"bump"} : ''}`;
+
+  useEffect(() => {
+    if (items.lenght === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <div>CartButton</div>
-  )
-}
+    <button className={btnClasses} onClick={props.onClick}>
+      <Link to="/shop/cart"className="cart-btn-icon">
+        <AiOutlineShoppingCart />
+      </Link>
+      <span>Your Cart</span>
+      <span className="badge">{numberOfCartItems}</span>
+    </button>
+  );
+};
 
-export default CartButton
+export default CartButton;
